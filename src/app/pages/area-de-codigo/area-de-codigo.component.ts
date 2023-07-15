6371309576f92e4533e8270e8228a703dc95413b
Karen  Nohemy <karencerrato.lopez16@gmail.com>
2023-07-14 23:49:37 -0600

    Reemplazo de iframe por text-area, en proceso el higthline

--- src/app/pages/area-de-codigo/area-de-codigo.component.ts
+++ src/app/pages/area-de-codigo/area-de-codigo.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-area-de-codigo',
   templateUrl: './area-de-codigo.component.html',
   styleUrls: ['./area-de-codigo.component.css']
 })
-export class AreaDeCodigoComponent {
+export class AreaDeCodigoComponent implements OnInit {
+  ngOnInit() {
+    const textAreas = document.getElementsByClassName('text-area');
+    const textAreasArray = Array.from(textAreas) as HTMLTextAreaElement[];
 
+    textAreasArray.forEach((textArea: HTMLTextAreaElement) => {
+      textArea.addEventListener('input', () => {
+        const highlightedText = this.highlightText(textArea.value, textArea.selectionStart);
+        textArea.innerHTML = highlightedText;
+      });
+    });
+  }
+
+  highlightText(text: string, cursorPosition: number): string {
+    const words = text.split(' ');
+    const highlightedWords = words.map(word => {
+      if (word.startsWith(text.slice(0, cursorPosition))) {
+        return `<span class="highlight">${word}</span>`;
+      } else {
+        return word;
+      }
+    });
+    return highlightedWords.join(' ');
+  }
 }
