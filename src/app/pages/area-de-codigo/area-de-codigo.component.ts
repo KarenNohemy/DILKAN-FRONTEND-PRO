dae42e552a41cec6ff02118632150484fa324089
Karen  Nohemy <karencerrato.lopez16@gmail.com>
2023-08-17 21:44:49 -0600

    Iframe funcional en tiempo real, el botón Run funciona como el boton guardar

--- src/app/pages/area-de-codigo/area-de-codigo.component.ts
+++ src/app/pages/area-de-codigo/area-de-codigo.component.ts
@@ -22,4 +22,41 @@ export class AreaDeCodigoComponent  {
     // utilizando HttpClient u otras técnicas de comunicación en Angular.
   }
 
+  // Propiedades para el contenido de los textareas y el iframe
+
+
+  // Método para actualizar el contenido del iframe
+  updateIframe(textareaId: string, content: string) {
+    if (textareaId === 'htmlArea') {
+      this.htmlContent = content;
+    } else if (textareaId === 'cssArea') {
+      this.cssContent = content;
+    } else if (textareaId === 'jsArea') {
+      this.jsContent = content;
+    }
+
+    const iframe = document.getElementById('pantalla') as HTMLIFrameElement;
+    const iframeDocument = iframe.contentWindow?.document;
+    if (iframeDocument) {
+      const combinedContent = `
+        <html>
+          <head>
+            <style>${this.cssContent}</style>
+            <script>${this.jsContent}</script>
+          </head>
+          <body>${this.htmlContent}</body>
+        </html>
+      `;
+      iframeDocument.open();
+      iframeDocument.write(combinedContent);
+      iframeDocument.close();
+    }
+  }
+
 }
+
+
+
+
+
+
