b22efa1e9bcf24cfc15c17ad6ea29c4b4b725921
Karen Nohemy Lopez Cerrato <karen.lopez@tigo.com.hn>
2023-08-23 17:59:36 -0600

    Retoques en el diseño

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-pages',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./pages.component.css'],
 })
 export class PagesComponent {
+  pageTitle: string = 'Mis Proyectos'; // Título predeterminado
 
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      // Obtén el título de los datos de la ruta activa y actualiza pageTitle
+      this.pageTitle = this.getTituloDesdeRuta(this.activatedRoute);
+    });
+  }
+
+  private getTituloDesdeRuta(ruta: ActivatedRoute): string {
+    while (ruta.firstChild) {
+      ruta = ruta.firstChild;
+    }
+    return ruta.snapshot.data['title'] || 'Mis Proyectos';
+  }
 }
